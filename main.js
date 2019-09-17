5e9dcac81548602586d525c0802b9612cbeef129
Alejandro Haro <harito16@me.com>
2019-09-17 17:16:01 -0500

    added questions as an object

---
+++ main.js
@@ -0,0 +1,184 @@
+let questionsOld = [
+    {
+        question: 'Why does Cain kill his brother Abel?',
+        answers: [
+            'God told him to', 
+            'Abel was getting the inheritance', 
+            'The angel said so', 
+            'God was more pleased by Abel\'s sacrifice than by Cain\'s'
+        ]
+    },
+    {
+        question: 'How does Jacob steal his brother, Esau\'s, inheritance rights?',
+        answers: [
+            'He stole the testament and re-wrote it', 
+            'He tricked his father with his mother\'s help', 
+            'He got a judge to do it for him', 
+            'By killing his brother'
+        ]
+    },
+    {
+        question: 'From which catastrophe does Joseph save Egypt?',
+        answers: [
+            'From the plagues of Egypt', 
+            'From the furious Red Sea', 
+            'From a sandstorm', 
+            'From famine'
+        ]
+    },
+    {
+        question: 'How does God first appear to Moses?',
+        answers: [
+            'As an angel', 
+            'As talking sand', 
+            'As a cloud', 
+            'As a burning bush'
+        ]
+    },
+    {
+        question: 'What object that God gives Moses allows him to perform signs and wonders?',
+        answers: [
+            'An arch', 
+            'A wooden staff', 
+            'A serpent', 
+            'The Ten Commandments'
+        ]
+    },
+    {
+        question: 'How does God feed the Israelites in the desert?',
+        answers: [
+            'Bread', 
+            'Fishes', 
+            'Manna', 
+            'Beef'
+        ]
+    },
+    {
+        question: 'Why does Moses break the stone tablets inscribed with God\'s commandments at Mount Sinai?',
+        answers: [
+            'He fell coming from the mountain', 
+            'His people told him to', 
+            'He didn\'t want to follow them', 
+            'His people were worshipping golden statues'
+        ]
+    },
+    {
+        question: 'Why does God curse the Israelities to wander the deser for forty years before entering the Promised Land?',
+        answers: [
+            'Israelities didn\'t have faith', 
+            'Israelities didn\'t know how to read a map', 
+            'Moses didn\'t know where to go',
+            'They lost their camels when exiting Egypt'
+        ]
+    },
+    {
+        question: 'Who betrays Samson to the Philistines?',
+        answers: [
+            'Ruth', 
+            'Rebecca', 
+            'Jezebel', 
+            'Delilah'
+        ]
+    },
+    {
+        question: 'Why does the prophet Nathan rebuke David?',
+        answers: [
+            'David went to war', 
+            'David commits murder', 
+            'David commits adultery', 
+            'David kills Goliath'
+        ]
+    },
+    {
+        question: 'What does David bring to Jerusalem to bless the religious city?',
+        answers: [
+            'A golden statue', 
+            'Holy bread', 
+            'The Ark of the Covenant', 
+            'A wooden staff'
+        ]
+    },
+    {
+        question: 'Who is Elisha?',
+        answers: [
+            'David\'s apprentice and successor', 
+            'Joseph\'s apprentice and successor', 
+            'Elijah\'s apprentice and successor', 
+            'Abraham\'s apprentice and successor'
+        ]
+    },
+    {
+        question: 'Which Jwish fesitval results form the events in Esther?',
+        answers: [
+            'Yom Kippur', 
+            'Purim', 
+            'Shabbat', 
+            'Hanukkah'
+        ]
+    },
+    {
+        question: 'What does King Solomon do in Israel?',
+        answers: [
+            'He builds a statue', 
+            'He bakes bread', 
+            'He brings treasures from around the world', 
+            'He builds the grand temple'
+        ]
+    },
+    {
+        question: 'What is one of the main criteria in Leviticus for living in the Israelite camp?',
+        answers: [
+            'To be married', 
+            'To be ceremonially clean', 
+            'To have children', 
+            'To know how to pray'
+        ]
+    },
+    {
+       question:  'Why does God reprimand Job?',
+       answers: [
+        'He didn\'t love his wife', 
+        'He ate too much meat', 
+        'He didn\'t pray everyday',
+        'He listened the advice of this frineds'
+       ]
+    },
+    {
+        question: 'What did King Solomon ask for from God?',
+        answers: [
+            'Solomon asked for riches',
+            'Solomon asked for gold',
+            'Solomon asked for a wife', 
+            'Solomon asked for wisdom'
+        ]
+    },
+    {
+        question: 'Who wrote this line \'The Lord is my Shepherd, I shall not want\'?',
+        answers: [
+            'Elijah',
+            'Abraham',
+            'King David',
+            'King Solomon'
+        ]
+    },
+    {
+        question: 'What does \'Eve\' mean?',
+        answers: [
+            'Daughter of God',
+            'Mother of all living',
+            'Disobedience', 
+            'Humble'
+        ]
+    },
+    {
+        question: 'Who married Abraham?',
+        answers: [
+            'Rebecca',
+            'Esther',
+            'Sarah',
+            'Ruth'
+        ]
+    }
+
+]
+
